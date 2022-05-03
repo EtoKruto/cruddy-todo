3123f9c4b12f1983fb0b875e14452e97304e700f
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-05-03 17:57:46 -0600

    Refactores readAll in index.js

--- datastore/index.js
+++ datastore/index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
+const Promise = require('bluebird');
+const readFilePromise = Promise.promisify(fs.readFile);
+
 var items = {};
 
 
@@ -11,52 +14,56 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  if (text !== undefined) {
-    counter.getNextUniqueId((error, id) => {
-      if(error) {
-        // console.log('${exports.dataDir}/${id}', exports.dataDir, '/', id);
-        // console.log('is this walk the dog?: ', text);
-        // console.log('is this walk the dog?: ', error);
-        throw('Error with getUniqueId');
-      } else {
-        fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
-          if (err) {
-            throw ('error writing counter');
-          } else {
-            callback(null, { id, text });
-          };
-        });
-      }
-    })
-  };
+  // return new Promise(function(resolve, reject) {
+
+    if (text !== undefined) {
+      counter.getNextUniqueId((error, id) => {
+        if (error) {
+          return callback(error)
+        } else {
+          fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+            if (err) {
+              return callback(error)
+            } else {
+              callback(null, { id, text });
+            };
+          });
+        }
+      })
+    };
+
+  // })
 };
 
 exports.readAll = (callback) => {
-  let arrOfTodoObjects = [];
-  fs.readdir(`${exports.dataDir}`, (err, files) => {
+
+  fs.readdir(exports.dataDir, (err, files) => {
     if (err) {
       console.log("Houston we have an error reading directory")
-      return arrOfTodoObjects;
-    } else {
-
-
-      files.forEach((file, index) => {
-        let fileName = file.split('.')
-        arrOfTodoObjects.push({id: fileName[0], text: fileName[0]});
-      });
-
-
-      callback(null, arrOfTodoObjects);
+      return callback(err)
     }
+    var data = _.map(files, (file) => {
+      var id = path.basename(file, '.txt');
+      var filepath = path.join(exports.dataDir, file);
+      return readFilePromise(filepath).then((fileData) => {
+        return {
+          id: id,
+          text: fileData.toString()
+        };
+      });
+    });
 
+    Promise.all(data).done((items) => {
+      callback(null, items);
+    });
 
   });
-
-
 };
 
+
 exports.readOne = (id, callback) => {
-  fs.readFile(`/Users/Robert/Desktop/Hackreactor/Course/rfp2204-cruddy-todo/test/testData/${id}.txt`, 'utf8', (err, data) => {
+  var filepath = path.join(exports.dataDir, `${id}.txt`);
+  fs.readFile(filepath, 'utf8', (err, data) => {
     if (err) {
       callback(err, undefined);
     } else {
@@ -66,15 +73,14 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  fs.readFile(`/Users/Robert/Desktop/Hackreactor/Course/rfp2204-cruddy-todo/test/testData/${id}.txt`,'utf8', (error, data) => {
+  var filepath = path.join(exports.dataDir, `${id}.txt`);
+  fs.readFile(filepath, 'utf8', (error, data) => {
     if (error) {
-      // console.log("err: ", error);
       callback(error, undefined);
     } else {
-      // console.log("data: ", data);
-      // console.log("exports.dataDir}/${id}.txt", `${exports.dataDir}/${id}.txt`)
-      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+      fs.writeFile(filepath, text, (err) => {
         if (err) {
+          console.log(err);
           callback(err, undefined);
         } else {
           callback(null, { id, text: text });
@@ -86,25 +92,24 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  fs.readFile(`/Users/Robert/Desktop/Hackreactor/Course/rfp2204-cruddy-todo/test/testData/${id}.txt`,'utf8', (error, data) => {
+
+  var filepath = path.join(exports.dataDir, `${id}.txt`);
+  fs.readFile(filepath, 'utf8', (error, data) => {
     if (error) {
       callback(error, undefined);
     } else {
-      fs.unlink(`/Users/Robert/Desktop/Hackreactor/Course/rfp2204-cruddy-todo/test/testData/${id}.txt`, (err, data) => {
+      fs.unlink(filepath, (err, data) => {
         if (err) {
-          // console.log(err);
           callback(err);
         } else {
           console.log('Deleted');
-          callback(err);
+          callback(null);
         }
       });
     }
   });
 };
 
-
-// fs.unlink(filePath, callbackFunction);
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
@@ -115,52 +120,3 @@ exports.initialize = () => {
     fs.mkdirSync(exports.dataDir);
   }
 };
-
-
-
-// console.log(files)
-// arrOfTodoObjects = files.map(file, () => {
-//   fs.readFile(file, (error, fileData) => {
-//     if (err) {
-//       console.log('halp me')
-//     } else {
-//       console.log("fileData", fileData)
-//       console.log("file", file)
-
-//       return { text: fileData, id: file };
-//     }
-//   })
-// });
-
-
-
-// fs.readFile(`${exports.dataDir}/${fileName[0]}.txt`, "utf8", (err, fileData) => {
-//   if (err) {
-//     console.log('error reading file');
-//   } else {
-//     // callback(null, Number(fileData));
-//     console.log("fileName[0]", fileName[0])
-//     console.log("file", fileData)
-
-//     console.log("Object", {id: fileName[0], text: fileData});
-//     // arrOfTodoObjects.push({id: fileName[0], text: fileData});
-//     arrOfTodoObjects.push(fileData);
-
-//   }
-// });
-
-// console.log("arrOfTodoObjects to be returned ",arrOfTodoObjects);
-
-
-
-
-
-// counter.readCounter((error, fileData) => {
-//   if (error) {
-//     throw('readCounter Error in readAll');
-//   } else {
-//     console.log(fileData);
-//     arrOfTodoObjects.push({id: fileName[0], text: fileData});
-//     console.log(arrOfTodoObjects);
-//   }
-// });
